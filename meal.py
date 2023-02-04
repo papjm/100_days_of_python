ea16db254263252ab868b4416fa90971711dbd4c
papjm <102404994+papjm@users.noreply.github.com>
2023-02-04 15:51:17 +0100

    Create meal.py

---
+++ meal.py
@@ -0,0 +1,15 @@
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+#Get the total number of items in list.
+num_items = len(names)
+random_choice =random.randint(0, num_items - 1)
+
+person_who_will_pay = names[random_choice]
+print(person_who_will_pay + " is going to buy the meal today!")
+
+
