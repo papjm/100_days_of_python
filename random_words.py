0a2923757b034f7e030be092803af54fe28d33f0
papjm <102404994+papjm@users.noreply.github.com>
2023-02-22 16:04:59 +0100

    Update random_words.py

--- random_words.py
+++ random_words.py
@@ -1,1 +1,10 @@
 word_list = ["ardvark","baboon","camel"]
+import random
+chosen_word = random.choice(word_list)
+
+input("Guess a letter").lower()
+for letter in chosen_word:
+  if letter == guess:
+    print("right")
+  else:
+    print("wrong")
