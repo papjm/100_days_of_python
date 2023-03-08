63303481bbdb94b365d268a11bde45c3523a7be2
papjm <102404994+papjm@users.noreply.github.com>
2023-03-08 08:11:10 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -2,3 +2,6 @@ programming_dictionary = {"Bug": "An erro in a program that prevents the program
 print(programming_dictionary["Bug"])
 # adding a key and value to a dictionary
 programming_dictionary["loop"]= "The action of doing something over and over again."
+#wipe an existing dictionary
+programming_dictionary={}
+print(programming_dictionary)
