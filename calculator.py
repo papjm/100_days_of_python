09e8b4a6baec82cd4d2971bf22b3939574aaf6e0
papjm <102404994+papjm@users.noreply.github.com>
2023-03-15 13:19:08 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,17 @@
 #Building a simple calculator to excercise my knowledge on python programming
+
+#Add
+def addition(n1, n2):
+  return n1 + n2
+
+#subtract
+def subtraction(n1, n2):
+  return n1 - n2
+
+#multiply
+def multiplication(n1, n2):
+  return n1 * n2
+
+#divide
+def division(n1, n2):
+  return n1 % n2
