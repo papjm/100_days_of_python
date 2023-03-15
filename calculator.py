db48358b46bfd60c4cccc8a7ae9660d96f139c01
papjm <102404994+papjm@users.noreply.github.com>
2023-03-15 15:28:50 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 #Building a simple calculator to excercise my knowledge on python programming
-
+#look into the code again and make it recursive
 #Add
 def Add(n1, n2):
   return n1 + n2
@@ -23,12 +23,15 @@ operations = {
   "/" : Divide
 }
 
-num1 = int(input("What is the first number?: ").title())
-num2 = int(input("What is the second number?: ").title())
+should_continue =  True
+while should_continue:
 
-for symbols in operation:
-  print(symbols)
-operation_symbols = input("Pick an operation from the line above")
-calculation_function = operations[operation_symbols]
-answer = calculation_function(num1, num2)
-print(answer)
+  num1 = int(input("What is the first number?: ").title())
+  num2 = int(input("What is the second number?: ").title())
+
+  for symbols in operation:
+    print(symbols)
+  operation_symbols = input("Pick an operation from the line above")
+  calculation_function = operations[operation_symbols]
+  answer = calculation_function(num1, num2)
+  print(answer)
