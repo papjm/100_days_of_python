565abedde420309c50b467c2bbba756c6e0d96d6
papjm <102404994+papjm@users.noreply.github.com>
2023-03-16 10:07:03 +0100

    Update black_jack.py

--- black_jack.py
+++ black_jack.py
@@ -3,3 +3,7 @@ def deal_card():
   cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]:
   card = random.choice(cards)
   return card
+#Hint: deal user and computer two cards each using deal_card()
+
+user_cards = []
+computer_cards = []
