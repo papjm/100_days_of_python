1105ab22e274845aa51ffaef246313bf92c804fc
papjm <102404994+papjm@users.noreply.github.com>
2023-03-23 15:52:56 +0100

    Update higher_lower.py

--- higher_lower.py
+++ higher_lower.py
@@ -302,12 +302,17 @@ data = [
     }
 ]
 
+def format_data(account):
+    """Format the account data into a joint sentence"""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
 account_a = random.choice(data)
 account_b = random.choice(data)
 
 if account_a == account_b:
     account_b = random.choice(data)
-
-account_name = account_a["name"]
-account_descr = account_a["description"]
-account_country = account_a["country"]
+    
+print(f"Compare A: {format_data(account_a)}")
+print(f"Compare B: {format_data(account_b)}")
