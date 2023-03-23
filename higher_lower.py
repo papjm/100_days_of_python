134f2d66dc4dffce71fe6e94bec76c1dddc2770c
papjm <102404994+papjm@users.noreply.github.com>
2023-03-23 15:42:08 +0100

    Update higher_lower.py

--- higher_lower.py
+++ higher_lower.py
@@ -307,3 +307,7 @@ account_b = random.choice(data)
 
 if account_a == account_b:
     account_b = random.choice(data)
+
+account_name = account_a["name"]
+account_descr = account_a["description"]
+account_country = account_a["country"]
