833148ba6800453a940385a9286bb41692ef1e47
papjm <102404994+papjm@users.noreply.github.com>
2023-03-27 16:27:51 +0100

    Update prettytable.py

--- prettytable.py
+++ prettytable.py
@@ -2,3 +2,7 @@ from prettytable import PrettyTable
 
 table = PrettyTable()
 print(table)
+
+table.add_column("pokemon name", ["pikachu", "squirtle", "charmander"])
+table.add_column("Type", ["electric", "water", "fire"])
+print(table)
