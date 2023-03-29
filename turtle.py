e802dd680347855260bfa0f9222550374c059d92
papjm <102404994+papjm@users.noreply.github.com>
2023-03-29 15:20:24 +0100

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,35 +1,19 @@
 #drawing graphics on the screen
-from turtle import Turtle, Screen
-
-timmy_the_turtle = Turtle()
-# timmy_the_turtle.shape("turtle")
-# timmy_the_turtle.color("red ")
-
-
-
-
-
-
-
-
-
-for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(90)
-
-#a for loop for drawing dotted lines.
+import random
 import turtle as t
 
 tim = t.Turtle()
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen","wheat", "SlateGray","SeaGreeb"]
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
 
-
-
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side_n)
 
 
 screen = Screen()
