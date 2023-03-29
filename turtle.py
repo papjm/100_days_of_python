b6136a38ec900d86f348e971a3b25cc53a471dfe
papjm <102404994+papjm@users.noreply.github.com>
2023-03-29 14:33:13 +0100

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,2 +1,24 @@
 #drawing graphics on the screen
-from turtle import Turtle
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
