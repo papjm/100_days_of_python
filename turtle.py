71456452b0411807d4e70cd92040e653c54d6cd2
papjm <102404994+papjm@users.noreply.github.com>
2023-03-29 15:02:42 +0100

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -17,7 +17,16 @@ for _ in range(4):
     timmy_the_turtle.forward(100)
     timmy_the_turtle.left(90)
 
+#a for loop for drawing dotted lines.
+import turtle as t
 
+tim = t.Turtle()
+
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
 
 
 
