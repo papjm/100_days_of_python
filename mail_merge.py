654237a206f312554a0fe6b01b36936117c81363
papjm <102404994+papjm@users.noreply.github.com>
2023-04-07 16:30:57 +0100

    Update mail_merge.py

--- mail_merge.py
+++ mail_merge.py
@@ -9,5 +9,7 @@ with open("./Input/Letters/starting_letter.txt") as letter_file:
     for name in names:
         stripped_name = name.strip()
         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        #saves each letter differently using the w(write) mode
         with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
             completed_letter.write(new_letter)
+
