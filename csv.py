3e26dc65460fd32f307b2e446843190dd07df219
papjm <102404994+papjm@users.noreply.github.com>
2023-04-10 14:21:16 +0100

    Update csv.py

--- csv.py
+++ csv.py
@@ -1,4 +1,16 @@
-#working with csv files(comma separated values:)
-with open("weather_data.csv") as data_file:
+import csv
+
+integer_list = []
+with open("weather-data.csv") as data_file:
     data = data_file.readlines()
-    print(data)
+    
+
+with open('weather-data.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        for item in row:
+            try:
+                integer_list.append(int(item))
+            except ValueError:
+                pass
+print(integer_list)
