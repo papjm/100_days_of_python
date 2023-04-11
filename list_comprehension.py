6b7989a4f87c9cc4b93159c0e42b28ded859a48a
papjm <102404994+papjm@users.noreply.github.com>
2023-04-11 14:46:36 +0100

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,3 +1,8 @@
 list = [1,2,3]
 new_list = [n+1 for n in list]
 print(new_list)
+#this line of code will double each integer in a range of integers
+
+list = range(1,5)
+new_list= [n*2 for n in list]
+print(new_list)
