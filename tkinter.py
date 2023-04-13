0242fcf6952845256a94acfa70d9cb43d49006ae
papjm <102404994+papjm@users.noreply.github.com>
2023-04-13 10:22:30 +0100

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -2,9 +2,24 @@ import tkinter
 
 window = tkinter.Tk()
 
-#keeps the window on the screen for users to interact with it
-window.mainloop()
-#title of program
+# keeps the window on the screen for users to interact with it
+
+# title of program
 window.title("my first GUI program")
-#adjust the gui screen size
+# adjust the gui screen size
 window.minsize(width=500, height=300)
+
+my_label = tkinter.Label(text="papjm from Hackerone", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
