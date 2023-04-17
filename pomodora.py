64968d446b7796c7cb491a6197f9db3f3f5c1a88
papjm <102404994+papjm@users.noreply.github.com>
2023-04-17 16:14:05 +0100

    Update pomodora.py

--- pomodora.py
+++ pomodora.py
@@ -26,6 +26,7 @@ window.config(padx=100, pady=50)
 canvas =  Canvas(width=200, height=224)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(103,112, image=tomato_img)
+canvas.create_text(103, 122, text="00:00")
 canvas.pack()
 
 
