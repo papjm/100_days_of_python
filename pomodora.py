3c602cdb30dd644e36b2e19973d468d9339aac28
papjm <102404994+papjm@users.noreply.github.com>
2023-04-17 16:05:58 +0100

    Create pomodora.py

---
+++ pomodora.py
@@ -0,0 +1,46 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+#pomodoro means tomatoe in italian
+window.title = ("pomodoro")
+
+
+
+canvas =  Canvas(width=200, height=224)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100,112, image=tomato_img)
+canvas.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
