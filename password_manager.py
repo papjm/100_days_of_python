bba0195bd0147c44786189adaaad77e8b4e4a85a
papjm <102404994+papjm@users.noreply.github.com>
2023-04-21 19:33:29 +0100

    Update password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -7,12 +7,17 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-
+window.config(padx=20,pady=20)
 canvas = Canvas(height=200,width=200)
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(image=logo_img)
+canvas.create_image(100,100,image=logo_img)
+canvas.pack()
 
 
+#Labels
+website_label = Label(text="Website")
+email_label = Label(text="Email/Username")
+password_label = Label(text="password")
 
 
 window.mainloop()
