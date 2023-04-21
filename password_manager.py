350eecbfb767c49287d7240592d38a2ad0288be5
papjm <102404994+papjm@users.noreply.github.com>
2023-04-21 19:49:36 +0100

    Update password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -15,12 +15,12 @@ canvas.pack()
 canvas.grid(row=0, column=1)
 
 #Labels
-website_label = Label(text="Website")
+website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
-email_label = Label(text="Email/Username")
+email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
-password_label = Label(text="password")
+password_label = Label(text="password:")
 password_label.grid(row=3, column=0)
 
 #Entries
@@ -29,5 +29,5 @@ website_entry.grid(row=1, column=1)
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1)
 password_entry = Entry(width=21)
-
+password_entry.grid(row=3, column=1)
 window.mainloop()
