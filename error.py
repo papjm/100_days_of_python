11388a8eabff06549bb41cdb3b59ef8ef98b31e0
papjm <102404994+papjm@users.noreply.github.com>
2023-04-26 16:50:12 +0100

    Update error.py

--- error.py
+++ error.py
@@ -2,3 +2,12 @@ try
 except
 else
 finally
+
+try:
+  file = open("a_file.txt")
+  a_dictionary = {"key": "value"}
+  print(a_dictionary[asdg])
+  
+except FileNotFoundError:
+  file = open("a_file.txt", "w")
+  file.write("Something")
